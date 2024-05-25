299fddb2a62442a3b006f67bd949092bb06e177a
Muazzi <mrawenxa@gmail.com>
2024-05-25 09:10:38 +0200

    added categories feature

---
+++ crwn-clothing/src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import './category-item.component.styles.scss';
+
+const CategoryItem = ({ category }) => {
+    const { title, imageUrl } = category;
+    return (
+        <div
+        className="category-container"
+        >
+        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+         <div className="category-image-container"></div>
+        
+        <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+        </div>
+       </div>
+    );
+}
+export default CategoryItem;
